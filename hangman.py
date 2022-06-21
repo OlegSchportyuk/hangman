73f3d0461de8b6000f5963d158618f0c16e67a1b
OlegSchportyuk <oleg.schportyuk@yandex.ru>
2022-06-21 17:33:19 +0300

    'hello'

---
+++ hangman.py
@@ -0,0 +1,43 @@
+import random
+
+def hangman(word):
+    wrong = 0
+    stages = ['',
+              '__________        ',
+              '|                 ',
+              '|          |      ',
+              '|          0      ',
+              '|         /|\     ',
+              '|         / \     ',
+              '|                 '
+              ]
+    rletters = list(word)
+    board = ['_']*len(word)
+    win = False
+    print('Добро пожаловать на казнь!')
+    while wrong < len(stages) - 1:
+        print('\n')
+        msg = 'Введите букву: '
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((' '.join(board)))
+        e = wrong +1
+        print('\n'.join(stages[0:e]))
+        if '_' not in board:
+            print('Вы выиграли! Было загадано слово: ')
+            print(' '.join(board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(stages[0:wrong]))
+        print('\nВы проиграли! Было загадано слово: {}'.format(word))
+
+words = ['собака','зебра','холодильник','гараж','облако','луна','австралия','лондон','роза','береза']
+word = random.choice(words)
+
+hangman(word)
